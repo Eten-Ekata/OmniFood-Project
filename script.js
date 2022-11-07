e5b9c4b8d4ef44490d76b075b31091d611724f82
Eten-Ekata <ekataelishaeten@gmail.com>
2022-11-07 15:58:01 +0100

    sticky header update

--- script.js
+++ script.js
@@ -40,12 +40,16 @@ const obs = new IntersectionObserver(
     const ent = entries[0];
     console.log(ent);
     if (ent.isIntersecting === false) {
-      document.querySelector(".header").classList.add("sticky");
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
     }
   },
   {
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEl);
